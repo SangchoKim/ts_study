e0f4a1aa60d916ccfa37160a5b6a9ced62d8bd0a
SangchoKim <wjdrms1919@gmail.com>
2020-07-01 02:26:41 +0900

    구구단 업데이

--- study/gugudan/lecture.js
+++ study/gugudan/lecture.js
@@ -1,9 +1,9 @@
 var numberOne = Math.ceil(Math.random() * 9);
 var numberTwo = Math.ceil(Math.random() * 9);
 var result = numberOne * numberTwo;
-var wordNumber = document.createElement("div");
-wordNumber.textContent = numberOne + " \uACF1\uD558\uAE30 " + numberTwo;
-document.body.append(wordNumber);
+var word = document.createElement("div");
+word.textContent = numberOne + " \uACF1\uD558\uAE30 " + numberTwo;
+document.body.append(word);
 var form = document.createElement("form");
 document.body.append(form);
 var input = document.createElement("input");
@@ -16,12 +16,12 @@ var resultDiv = document.createElement("div");
 document.body.append(resultDiv);
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-    if (result === parseInt(input.value)) {
+    if (result === +input.value) {
         resultDiv.textContent = "딩동댕";
         numberOne = Math.ceil(Math.random() * 9);
         numberTwo = Math.ceil(Math.random() * 9);
         result = numberOne * numberTwo;
-        wordNumber.textContent = numberOne + " \uACF1\uD558\uAE30 " + numberTwo;
+        word.textContent = numberOne + " \uACF1\uD558\uAE30 " + numberTwo;
         input.value = "";
         input.focus();
     }
--- study/gugudan/lecture.ts
+++ study/gugudan/lecture.ts
@@ -3,9 +3,9 @@ let numberTwo = Math.ceil(Math.random() * 9);
 
 let result = numberOne * numberTwo;
 
-const wordNumber = document.createElement("div");
-wordNumber.textContent = `${numberOne} 곱하기 ${numberTwo}`;
-document.body.append(wordNumber);
+const word = document.createElement("div");
+word.textContent = `${numberOne} 곱하기 ${numberTwo}`;
+document.body.append(word);
 
 const form = document.createElement("form");
 document.body.append(form);
@@ -23,12 +23,12 @@ document.body.append(resultDiv);
 
 form.addEventListener("submit", e => {
   e.preventDefault();
-  if (result === parseInt(input.value)) {
+  if (result === +input.value) {
     resultDiv.textContent = "딩동댕";
     numberOne = Math.ceil(Math.random() * 9);
     numberTwo = Math.ceil(Math.random() * 9);
     result = numberOne * numberTwo;
-    wordNumber.textContent = `${numberOne} 곱하기 ${numberTwo}`;
+    word.textContent = `${numberOne} 곱하기 ${numberTwo}`;
     input.value = "";
     input.focus();
   } else {
